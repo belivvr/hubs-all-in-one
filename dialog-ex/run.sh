5f620f86244e19bebecd218ba042ed38d360c249
Your Name <you@example.com>
2023-03-27 14:29:50 +0900

    .

--- dialog-ex/run.sh
+++ dialog-ex/run.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 set -e
 cd "$(dirname "$0")"
-. ../.env
 
-sh ./check_cert_expiration.sh
-sh ./initialize_hostnames.sh
-
-cd $WORKDIR/dialog
-
-mkdir -p certs
-
-cp $SSL_CERT_FILE ./certs/fullchain.pem
-cp $SSL_KEY_FILE ./certs/privkey.pem
-cp $PERMS_PUB_FILE ./certs/perms.pub.pem
-
-export MEDIASOUP_LISTEN_IP=0.0.0.0
-export MEDIASOUP_ANNOUNCED_IP=0.0.0.0
+# .env가 현재 경로를 기준으로 파일을 가져온다.
+cd ..
+. ./.env
+cd "$(dirname "$0")"
 
 docker rm -f dialog
-docker rmi dialog
-docker build -t dialog .
-docker run -d --name dialog dialog npm start
-# npm start
+
+docker run -d --name dialog \
+-v $SSL_CERT_FILE:/app/certs/fullchain.pem \
+-v $SSL_KEY_FILE:/app/certs/privkey.pem \
+-v $PERMS_PUB_FILE:/app/certs/perms.pub.pem \
+-p $DIALOG_PORT:4443 \
+dialog
+
+docker logs dialog
