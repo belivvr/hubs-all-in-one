d06841447256bd05292101dff374f91ca4f992b3
Your Name <you@example.com>
2023-04-10 17:05:42 +0900

    .

--- proxy/run.sh
+++ proxy/run.sh
@@ -24,3 +24,4 @@ docker logs proxy
 
 # curl "https://hubs.vevv.io:4080/http://hubs.vevv.io:9000/abc123?query=123&test=abc#1234"
 curl "https://hubs-proxy.vevv.io:4080/https://uploads-prod.reticulum.io:443/files/728260ab-4e8f-4052-8e1a-c7fae3492989.glb"
+# curl "https://hubs-proxy.vevv.io:4080"
