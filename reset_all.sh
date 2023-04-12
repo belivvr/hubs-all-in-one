63edbcafde557f5e0222338f46f244683699447d
Your Name <you@example.com>
2023-04-12 22:33:56 +0900

    .

--- reset_all.sh
+++ reset_all.sh
@@ -6,23 +6,23 @@ docker rm -f hubs-front
 docker rm -f hubs-admin
 docker rm -f db.vevv.io
 
-docker rmi postgrest
-docker rmi dialog
-docker rmi nginx:stable-alpine
-docker rmi reticulum
-docker rmi hubs
-docker rmi hubs
-docker rmi postgres:11-bullseye
-
-docker images -a | grep "<none>" | awk '{print $3}' | xargs docker rmi
 docker volume ls -q | xargs docker volume rm
 
-docker system prune -f
+# docker rmi postgrest
+# docker rmi dialog
+# docker rmi nginx:stable-alpine
+# docker rmi reticulum
+# docker rmi hubs
+# docker rmi hubs
+# docker rmi postgres:11-bullseye
 
 sh dialog-ex/build.sh
 sh postgrest/build.sh
 sh hubs-ex/build.sh
 sh reticulum-ex/build.sh
+
+docker images -a | grep "<none>" | awk '{print $3}' | xargs docker rmi
+docker system prune -f
 
 sh db/run.sh
 sh dialog-ex/run.sh
