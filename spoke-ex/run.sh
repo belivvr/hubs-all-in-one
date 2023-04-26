97d8a2a34982369402a6a2326e23e348a180ccc5
Your Name <you@example.com>
2023-04-26 14:45:04 +0900

    .

--- spoke-ex/run.sh
+++ spoke-ex/run.sh
@@ -8,18 +8,10 @@ cd ..
 cd $THISDIR
 
 docker rm -f spoke
+
 docker run -d --name spoke \
--v $SSL_CERT_FILE:/app/spoke/certs/cert.pem \
--v $SSL_KEY_FILE:/app/spoke/certs/key.pem \
--w /app/spoke \
+-v $SSL_CERT_FILE:/etc/nginx/certs/cert.pem \
+-v $SSL_KEY_FILE:/etc/nginx/certs/key.pem \
+-v $(pwd)/nginx.conf.spoke:/etc/nginx/nginx.conf \
 -p 9090:9090 \
--e RETICULUM_SERVER="$HUBS_HOST:4000" \
--e HUBS_SERVER="$HUBS_HOST:4000" \
--e ROUTER_BASE_PATH="/spoke" \
--e NODE_TLS_REJECT_UNAUTHORIZED=0 \
--e BASE_ASSETS_PATH="https://$HUBS_HOST:9090/" \
--e CORS_PROXY_SERVER="$PROXY_HOST:4080" \
--e NON_CORS_PROXY_DOMAINS="$PROXY_HOST,$HUBS_HOST" \
--e INTERNAL_HOSTNAME="$HUBS_HOST" \
--e THUMBNAIL_SERVER="$THUMBNAIL_HOST" \
-spoke sh -c "yarn start"
+spoke
