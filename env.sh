1eef70341fc4ad9c669bb7d46ade119814581d40
Your Name <you@example.com>
2023-08-03 17:05:01 +0900

    fix: reticulum build error

--- env.sh
+++ env.sh
@@ -1,57 +1,43 @@
 #!/bin/bash
+set -e
+
 CERTS="$(pwd)/certs"
-SUFFIX="xrcloud.vevv.io"
+SUFFIX="1.vevv.io"
 
 SOURCE=(
-    "SSL_CERT_FILE"
-    "SSL_KEY_FILE"
-    "PERMS_PRV_FILE"
-    "PERMS_PUB_FILE"
-    "PERMS_JWK_FILE"
-    "HUBS_HOST"
-    "PROXY_HOST"
-    "DB_HOST"
-    "DB_USER"
-    "DB_PASSWORD"
-    "POSTGREST_HOST"
-    "DIALOG_HOST"
-    "DIALOG_PORT"
-    "THUMBNAIL_HOST"
+    SSL_CERT_FILE="$CERTS/vevv.io_202303247.unified.crt.pem"
+    SSL_KEY_FILE="$CERTS/vevv.io_202303247.key.pem"
+    PERMS_PRV_FILE="$CERTS/perms.prv.pem"
+    PERMS_PUB_FILE="$CERTS/perms.pub.pem"
+    PERMS_JWK_FILE="$CERTS/perms-jwk.json"
+    HUBS_HOST="hubs-${SUFFIX}"
+    PROXY_HOST="proxy-${SUFFIX}"
+    DB_HOST="db-${SUFFIX}"
+    DB_USER="xrcloud"
+    DB_PASSWORD="xrcloud123!"
+    POSTGREST_HOST="postgrest-${SUFFIX}"
+    DIALOG_HOST="dialog-${SUFFIX}"
+    DIALOG_PORT="4443"
+    THUMBNAIL_HOST="thumbnail-${SUFFIX}"
      #입장과 퇴장시 이벤트 로그를 남겨줄 API를 지정한다. 레티큘럼에서만 사용.
-    "EVENT_ENTER_URL"
-    "EVENT_EXIT_URL"
-)
-TARGET=(
-    "$CERTS/vevv.io_202303247.unified.crt.pem"
-    "$CERTS/vevv.io_202303247.key.pem"
-    "$CERTS/perms.prv.pem"
-    "$CERTS/perms.pub.pem"
-    "$CERTS/perms-jwk.json"
-    "hubs-${SUFFIX}"
-    "proxy-${SUFFIX}"
-    "db-${SUFFIX}"
-    "xrcloud"
-    "xrcloud123!"
-    "postgrest-${SUFFIX}"
-    "dialog-${SUFFIX}"
-    "4443"
-    "thumbnail-${SUFFIX}"
-    "xrcloud의 로그 생성 url"
-    "xrcloud의 로그 삭제 url"
+    EVENT_ENTER_URL="xrcloud의 로그 생성 url"
+    EVENT_EXIT_URL="xrcloud의 로그 삭제 url"
 )
 
 array_length=${#SOURCE[@]}
 
-for ((i=0; i<array_length; i++)); do
-    export ${SOURCE[i]}=${TARGET[i]}
-done
-
 function replace_vars_in_files() {
     FILE=$1
 
-    for ((i=0; i<array_length; i++)); do
-        # ESCAPED_DIR="${CURRENT_DIR//\//\\/}"
-        # 문자열 내에 /를 \/로 변환한다.
-        sed -i "s/\${${SOURCE[i]}}/${TARGET[i]//\//\\/}/g" $FILE
+    for ((i = 0; i < array_length; i++)); do
+
+        IFS="=" read -r NAME VALUE <<<"${SOURCE[i]}"
+        echo "$NAME $VALUE $FILE"
+        sed -i "s/\${${NAME}}/${VALUE//\//\\/}/g" $FILE
     done
 }
+
+for ((i = 0; i < array_length; i++)); do
+    export "${SOURCE[i]}"
+    echo "${SOURCE[i]}"
+done
