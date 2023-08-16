cc4dfbd82777c5c2b6cee37ae7f22d979948dc21
gudrb33333 <gudrb33333@gmail.com>
2023-08-16 11:48:08 +0900

    개발 환경으로 env.sh 수정

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 set -e
 
 CERTS="$(pwd)/certs"
-SUFFIX="room.xrcloud.app"
+SUFFIX="dev-haio.vevv.io"
 
 SOURCE=(
     SSL_CERT_FILE="$CERTS/room.xrcloud.app_20230809AAD35.unified.crt.pem"
@@ -11,21 +11,21 @@ SOURCE=(
     PERMS_PUB_FILE="$CERTS/perms.pub.pem"
     PERMS_JWK_FILE="$CERTS/perms-jwk.json"
     HUBS_HOST="${SUFFIX}"
-    PROXY_HOST="${SUFFIX}"
-    DB_HOST="${SUFFIX}"
+    PROXY_HOST="proxy-${SUFFIX}"
+    DB_HOST="db-${SUFFIX}"
     DB_USER="xrcloud"
     DB_PASSWORD="ENrrYdFyD3f"
-    POSTGREST_HOST="${SUFFIX}"
-    DIALOG_HOST="${SUFFIX}"
+    POSTGREST_HOST="postgrest-${SUFFIX}"
+    DIALOG_HOST="dialog-${SUFFIX}"
     DIALOG_PORT="4443"
-    THUMBNAIL_HOST="${SUFFIX}"
+    THUMBNAIL_HOST="thumbnail-${SUFFIX}"
      #입장과 퇴장시 이벤트 로그를 남겨줄 API를 지정한다. 레티큘럼에서만 사용.
     EVENT_ENTER_URL="https://xrcloud.app/api"
     EVENT_EXIT_URL="https://xrcloud.app/api"
-    DB_VOLUME_DIR="/data3/postgres"
-    DB_NAS_LOCATION="169.254.84.53:/n3048487_HaioProdDB"
+    DB_VOLUME_DIR="/data/postgres"
+    DB_NAS_LOCATION="169.254.84.53:/n3048487_HaioDevDB"
     RETICULUM_STORAGE_DIR="/storage"
-    STORAGE_NAS_LOCATION="169.254.84.53:/n3048487_HaioProdStorage"
+    STORAGE_NAS_LOCATION="169.254.84.53:/n3048487_HaioDevStorage"
 )
 
 array_length=${#SOURCE[@]}
@@ -45,3 +45,4 @@ for ((i = 0; i < array_length; i++)); do
     export "${SOURCE[i]}"
     echo "${SOURCE[i]}"
 done
+
