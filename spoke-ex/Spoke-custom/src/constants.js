a4c6966c184fffa071e72c031d2c8481b8e44931
benny <vvsogi@gmail.com>
2023-08-28 17:57:15 +0900

    xrcloud #4, Add favicon

---
+++ spoke-ex/Spoke-custom/src/constants.js
@@ -0,0 +1,9 @@
+const PRIVACY = "https://www.mozilla.org/en-US/privacy/hubs/";
+const TERMS = "https://www.mozilla.org/en-US/about/legal/terms/hubs/";
+const serverUrl = 'https://vevv-test.vevv.io:3300';
+
+module.exports = {
+  PRIVACY,
+  TERMS,
+  serverUrl
+};
