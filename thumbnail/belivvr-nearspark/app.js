a3201a4966967c98192593fddeb7ee312b6acd99
benny <vvsogi@gmail.com>
2024-01-03 18:15:10 +0900

    creates proxy, thumbnail networks name haio. separate nearspark

---
+++ thumbnail/belivvr-nearspark/app.js
@@ -0,0 +1,35 @@
+const lambda = require("./index");
+const express = require("express");
+
+const app = express();
+
+// app.get('/_healthz', function (req, res) {
+//   res.send('1');
+// });
+
+app.get("/thumbnail/:b64url", function(req, res) {
+  try{
+    lambda.handler(
+      {
+        queryStringParameters: req.query,
+        pathParameters: {
+          url: req.url.replace("/thumbnail/", "")
+        }
+      },
+      null,
+      async function(something, callback) {
+
+        res
+          .status(callback.statusCode)
+          .header(callback.headers)
+          .send(Buffer.from(callback.body, 'base64'))
+      }
+    )
+  } catch (error){
+    res.status(400)
+  }
+});
+
+app.listen(5000, function() {
+  console.log("listening on :5000");
+});
